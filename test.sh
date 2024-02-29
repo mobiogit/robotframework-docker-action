707f6bc8bacf85e4d405f7040b507a0cc6aeba96
Mike Noskov <51156475+mobiogit@users.noreply.github.com>
2024-02-29 14:53:47 -0500

    Code cleanup

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 # Create reports folder
-TESTS_DIR=$(pwd)/$ROBOT_TESTS_DIR
+REPORTS_DIR=$(pwd)/$ROBOT_REPORTS_DIR
 sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
 
 docker run --shm-size=$ALLOWED_SHARED_MEMORY \
@@ -7,7 +7,7 @@ docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e ROBOT_THREADS=$ROBOT_THREADS \
   -e PABOT_OPTIONS="$PABOT_OPTIONS" \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
-  -e ROBOT_TESTS_DIR="/opt/robotframework/tests/$TESTS_DIR" \
+  -e ROBOT_TESTS_DIR="/opt/robotframework/tests/$ROBOT_TESTS_DIR" \
   -v $REPORTS_DIR:/opt/robotframework/reports:Z \
   -v .:/opt/robotframework/tests:Z \
   --user $(id -u):$(id -g) \
