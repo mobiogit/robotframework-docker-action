65c0cc46f2ca3f2c90d54370a475f7f3f243bfd7
Mike Noskov <51156475+mobiogit@users.noreply.github.com>
2024-02-29 14:44:44 -0500

    fix tests dir path

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e ROBOT_THREADS=$ROBOT_THREADS \
   -e PABOT_OPTIONS="$PABOT_OPTIONS" \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
-  -e ROBOT_TESTS_DIR="$TESTS_DIR" \
+  -e ROBOT_TESTS_DIR="/opt/robotframework/tests/$TESTS_DIR" \
   -v $REPORTS_DIR:/opt/robotframework/reports:Z \
   -v .:/opt/robotframework/tests:Z \
   --user $(id -u):$(id -g) \
